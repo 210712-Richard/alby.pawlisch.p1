7164aef65f4920523f0699febea9d12cee56c289
Sophia Pawlisch <300040900@fvtc.edu>
2021-07-30 10:14:34 -0500

    pre-class stuff

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,5 +1,15 @@
 package com.revature;
 
+import io.javalin.Javalin;
+
 public class Driver {
+	public static void main(String[] args) {
+		
+		Javalin app = Javalin.create().start(8080);
+		
+		app.get("/", (ctx)->ctx.html("Hello Project 1"));
+	}
+	
+	
 
 }
